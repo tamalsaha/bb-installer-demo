c2513f3d4254dfc5117fe1d5154dec2b4f81abed
Tamal Saha <tamal@appscode.com>
2022-06-12 09:07:12 -0700

    added-all

--- sub-struct-demo/main.go
+++ sub-struct-demo/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name string `json:"name,omitempty"`
+	Age  int    `json:"age,omitempty"`
+}
+
 func main() {
-	
+	m := map[string]interface{}{
+		"persons": []Person{
+			{
+				Name: "John",
+				Age:  20,
+			},
+			{
+				Name: "Jane",
+				Age:  24,
+			},
+		},
+	}
+	data, err := json.Marshal(m)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(data))
 }
